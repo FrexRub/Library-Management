35fbfc4b16bafffd9e85c1bb9507e24d6ad5dffa
Станислав Рубцов <frex@mail.ru>
2025-01-17 17:05:20 +0300

    edit endpoint create(validater)

--- src/users/routers.py
+++ src/users/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Form, Request, Response, status
+from fastapi import APIRouter, Depends, Request, Response, status
 from fastapi.exceptions import HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
@@ -7,7 +7,7 @@ from starlette.responses import Response
 
 from src.core.config import COOKIE_NAME
 from src.core.database import get_async_session
-from src.core.exceptions import ExceptDB, NotFindUser, EmailInUse, ExceptUser
+from src.core.exceptions import ErrorInData, NotFindUser, EmailInUse, ExceptUser
 from src.core.jwt_utils import create_jwt, set_cookie, validate_password
 from src.users.crud import get_user_from_db, create_user
 from src.users.depends import current_superuser_user
@@ -19,28 +19,29 @@ router = APIRouter(prefix="/users", tags=["User"])
 
 @router.post("/create", response_class=Response, status_code=status.HTTP_201_CREATED)
 async def user_registration(
-    request: Request,
     user: UserCreateSchemas,
     session: AsyncSession = Depends(get_async_session),
 ) -> Response:
     try:
         result: int = await create_user(session=session, user_data=user)
     except EmailInUse:
-        return Response(content="The email address is already in use", status_code=400)
-    except ExceptUser:
-        return Response(
-            content="The user with the username: %s is already registered"
-            % user.username,
-            status_code=400,
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The email address is already in use",
         )
+    except ExceptUser:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"The user with the username: {user.username} is already registered",
+        )
+    except ErrorInData:
+        pass
     else:
         return Response(status_code=status.HTTP_201_CREATED, content="Ok")
 
 
 @router.post("/login", response_class=Response, status_code=200)
-async def userlogin(
-    request: Request, data_login: LoginSchemas, session=Depends(get_async_session)
-):
+async def userlogin(data_login: LoginSchemas, session=Depends(get_async_session)):
     try:
         user: User = await get_user_from_db(
             session=session, username=data_login.username
@@ -63,3 +64,10 @@ async def userlogin(
 @router.get("/list", status_code=200)
 async def get_list_users(session=Depends(get_async_session)):
     pass
+
+
+# @router.get("/", response_model=list[Product])
+# async def ger_products(
+#     session: AsyncSession = Depends(db_helper.scope_session_dependency),
+# ):
+#     return await crud.get_products(session=session)
--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,28 +1,41 @@
 from datetime import datetime
+import re
 
-from pydantic import BaseModel, EmailStr, ConfigDict, Field
+from pydantic import BaseModel, EmailStr, ConfigDict, Field, field_validator
+
+PATTERN_PASSWORD = (
+    r'^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!"#\$%&\(\)\*\+,-\.\/:;<=>\?@[\]\^_'
+    r"`\{\|}~])[a-zA-Z0-9!\$%&\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]{8,}$"
+)
 
 
-class UserIdSchemas(BaseModel):
-    id: int
-
-
-class UserCreateSchemas(BaseModel):
-    username: str
+class UserBaseSchemas(BaseModel):
+    username: str = Field(min_length=2)
     first_name: str
     last_name: str
     email: EmailStr
+
+
+class UserCreateSchemas(UserBaseSchemas):
     hashed_password: str = Field(alias="password")
 
+    @field_validator("username")
+    def username_alphanumeric(cls, value):
+        if not re.match(r"^[a-zA-Z0-9_]+$", value):
+            raise ValueError("Username must be alphanumeric")
+        return value
 
-class OutUserSchemas(BaseModel):
+    @field_validator("hashed_password")
+    def validate_password(cls, value):
+        if not re.match(PATTERN_PASSWORD, value):
+            raise ValueError("Invalid password")
+        return value
+
+
+class OutUserSchemas(UserBaseSchemas):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
-    username: str
-    first_name: str
-    last_name: str
-    email: EmailStr
     registered_at: datetime
 
 
