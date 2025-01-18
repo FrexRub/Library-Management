972496722acf02521e0ad26d80464f498a929504
Станислав Рубцов <frex@mail.ru>
2025-01-18 08:45:18 +0300

    rename dir books to authors

---
+++ src/authors/schemas.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from datetime import date
+
+from pydantic import BaseModel, ConfigDict
+
+
+class AuthorBaseSchemas(BaseModel):
+    full_name: str
+    biography: str
+    date_birth: date
+
+
+class AuthorCreateSchemas(AuthorBaseSchemas):
+    pass
+
+
+class OutAuthorSchemas(BaseModel):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
