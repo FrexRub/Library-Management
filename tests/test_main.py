95b5b155ed0d7f6c7d8d64aed05e87649768789a
Станислав Рубцов <frexrub@gmail.com>
2025-01-20 17:32:07 +0300

    add new tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,17 @@
-import os
-from typing import Optional
-
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 
-# from src import models
+from src.users.models import User
+from src.core.jwt_utils import create_jwt
+from src.core.config import COOKIE_NAME
+
+username_admin = "SRub"
+email_admin = "frexxx@mail.ru"
+password_admin = "1qaz!QAZ"
+
+username = "Bob"
+email = "Bob@mail.ru"
+password = "1qaz!QAZ"
 
 
 async def test_get_client(client: AsyncClient):
@@ -13,130 +19,116 @@ async def test_get_client(client: AsyncClient):
     assert response.status_code == 200
 
 
+async def test_create_user(client: AsyncClient):
+    user = {
+        "username": username_admin,
+        "first_name": "",
+        "last_name": "",
+        "email": email_admin,
+        "password": password_admin,
+    }  # Данные для полей формы
+    response = await client.post("/users/create", json=user)
+    assert response.status_code == 201
+    assert response.json()["id"] == 1
+
+
+async def test_create_user_bad_email(client: AsyncClient):
+    user = {
+        "username": username,
+        "first_name": "",
+        "last_name": "",
+        "email": email_admin,
+        "password": password,
+    }  # Данные для полей формы
+    response = await client.post("/users/create", json=user)
+    assert response.status_code == 400
+    assert response.json()["detail"] == "The email address is already in use"
+
+
+async def test_set_user_admin(db_session: AsyncSession):
+    user = await db_session.get(User, 1)
+    user.is_superuser = True
+    await db_session.commit()
+    assert user.is_superuser
+
+
+async def test_login_user(client: AsyncClient):
+    user = {
+        "username": username_admin,
+        "password": password_admin,
+    }  # Данные для полей формы
+    response = await client.post("/users/login", json=user)
+    print(response.content)
+    assert response.status_code == 202
+    assert response.cookies.get(COOKIE_NAME) != None
+
+
+async def test_create_new_post(client: AsyncClient):
+    jwt: str = create_jwt("1")
+    cookies = {COOKIE_NAME: jwt}
+    response = await client.get("/users/list", cookies=cookies)
+
+    assert response.status_code == 200
+
+
+# async def test_get_main(client: AsyncClient):
+#     response = await client.get("/posts")
+#     assert response.status_code == 307
 #
-# async def test_get_user_id(client: AsyncClient):
-#     headers = {"api-key": "test"}
-#     response = await client.get("/api/users/1", headers=headers)
+#
+# async def test_create_user(db_session: AsyncSession):
+#     hash_password = create_hash_password(PASSWORD).decode()
+#     user: User = User(
+#         username=USERNAME,
+#         email=EMAIL,
+#         hashed_password=hash_password,
+#         is_active=True,
+#         is_superuser=False,
+#     )
+#     assert user.username == USERNAME
+#     num: int = await add_user_to_db(db_session, user)
+#     assert num == 1
+#
+#
+# async def test_create_user_raises(db_session: AsyncSession):
+#     hash_password = create_hash_password(PASSWORD).decode()
+#     user: User = User(
+#         username=USERNAME,
+#         email=EMAIL,
+#         hashed_password=hash_password,
+#         is_active=True,
+#         is_superuser=False,
+#     )
+#
+#     with pytest.raises(ExceptDB):
+#         await add_user_to_db(db_session, user)
+#
+#
+# def test_create_jwt():
+#     jwt: str = create_jwt("1")
+#     assert jwt.count(".") == 2
+#
+#
+# async def test_create_new_post(client: AsyncClient):
+#     # В случае успеха в эндпоинте идет переадресация на страницу index.html
+#     jwt: str = create_jwt("1")
+#     cookies = {COOKIE_NAME: jwt}
+#     post = {"title": "Test", "content": "Test post"}
+#     response = await client.post("/posts", data=post, cookies=cookies)
+#     assert response.status_code == 307
+#
+#
+# async def test_endpoint_test(client: AsyncClient):
+#     post = {"title": "Test", "content": "Test post"}  # Данные для полей формы
+#     jwt: str = create_jwt("1")
+#     cookies = {COOKIE_NAME: jwt}  # Генерация куков для авторизации
+#     response = await client.post("/posts/test", data=post, cookies=cookies)
 #     assert response.status_code == 200
-#     assert response.json()["user"]["name"] == "Ivan"
+#     assert response.json()["title"] == "Test"
 #
 #
-# async def test_post_tweet(client: AsyncClient):
-#     headers = {"api-key": "test"}
-#     tweet = {"tweet_data": "Hi", "tweet_media_ids": [0]}
-#     response = await client.post("/api/tweets", headers=headers, json=tweet)
-#     assert response.status_code == 201
-#     assert response.json()["tweet_id"] == 1
-#
-#
-# async def test_post_tweet_db(event_loop, db_session: AsyncSession):
-#     query = await db_session.execute(
-#         select(models.Tweet).where(models.Tweet.id == 1)
-#     )
-#     data_tweet: models.Tweet = query.scalars().first()
-#     assert data_tweet.tweet_data == "Hi"
-#
-#
-# async def test_upload_file(client: AsyncClient):
-#     path_dir = os.path.dirname(os.path.abspath(__file__))
-#     file_name = os.path.join(path_dir, "test_file.jpg")
-#     headers = {"api-key": "test"}
-#     files = {"file": (file_name, open(file_name, "rb"), "multipart/form-data")}
-#     response = await client.post("/api/medias", headers=headers, files=files)
-#     assert response.status_code == 201
-#
-#
-# async def test_upload_file_db(event_loop, db_session: AsyncSession):
-#     query = await db_session.execute(
-#         select(models.TweetMedia).where(models.TweetMedia.media_id == 1)
-#     )
-#     data_file: models.TweetMedia = query.scalars().first()
-#     assert "test_file.jpg" in data_file.name_file
-#
-#
-# async def test_post_likes_tweet(client: AsyncClient):
-#     headers = {"api-key": "test1"}
-#     response = await client.post(
-#         "/api/tweets/1/likes",
-#         headers=headers,
-#     )
-#     assert response.status_code == 201
-#     assert response.json()["rusult"]
-#
-#
-# async def test_post_likes_tweet_db(event_loop, db_session: AsyncSession):
-#     query = await db_session.execute(select(models.LikesTweet))
-#     data_tweet_like: models.LikesTweet = query.scalars().first()
-#     assert data_tweet_like.tweet_id == 1
-#
-#
-# async def test_get_tweet(client: AsyncClient):
-#     headers = {"api-key": "test"}
-#     response = await client.get("/api/tweets", headers=headers)
-#     assert response.status_code == 200
-#     assert response.json()["tweets"][0]["id"] == 1
-#
-#
-# async def test_delete_likes_twee_bad(client: AsyncClient):
-#     headers = {"api-key": "test"}
-#     response = await client.delete("/api/tweets/1/likes", headers=headers)
-#     assert response.status_code == 400
-#     assert not response.json()["rusult"]
-#
-#
-# async def test_delete_likes_twee(client: AsyncClient):
-#     headers = {"api-key": "test1"}
-#     response = await client.delete("/api/tweets/1/likes", headers=headers)
-#     assert response.status_code == 200
-#     assert response.json()["rusult"]
-#
-#
-# async def test_delete_tweet(client: AsyncClient):
-#     headers = {"api-key": "test"}
-#     response = await client.delete("/api/tweets/1", headers=headers)
-#     assert response.status_code == 200
-#     assert response.json()["rusult"]
-#
-#
-# async def test_delete_tweet_db(event_loop, db_session: AsyncSession):
-#     query = await db_session.execute(
-#         select(models.Tweet).where(models.Tweet.id == 1)
-#     )
-#     data_tweet: Optional[models.Tweet] = query.scalars().one_or_none()
-#     assert data_tweet is None
-#
-#
-# async def test_post_users_follow(client: AsyncClient):
-#     headers = {"api-key": "test3"}
-#     response = await client.post(
-#         "/api/users/1/follow",
-#         headers=headers,
-#     )
-#     assert response.status_code == 201
-#     assert response.json()["rusult"]
-#
-#
-# async def test_post_users_follow_db(event_loop, db_session: AsyncSession):
-#     query = await db_session.execute(
-#         select(models.followers).where(models.followers.c.user_id == 4)
-#     )
-#     data_follow: models.followers = query.scalars().one_or_none()
-#     assert data_follow is not None
-#
-#
-# async def test_delete_users_follow(client: AsyncClient):
-#     headers = {"api-key": "test3"}
-#     response = await client.delete(
-#         "/api/users/1/follow",
-#         headers=headers,
-#     )
-#     assert response.status_code == 200
-#     assert response.json()["rusult"]
-#
-#
-# async def test_delete_users_follow_db(event_loop, db_session: AsyncSession):
-#     query = await db_session.execute(
-#         select(models.followers).where(models.followers.c.user_id == 4)
-#     )
-#     data_follow: models.followers = query.scalars().one_or_none()
-#     assert data_follow is None
+# async def test_add_new_post_bd(db_session: AsyncSession):
+#     post: PostCreate = PostCreate(title="Test", body="Test post")
+#     await add_new_post(session=db_session, post=post, id_user=1)
+#     post_db: Post = await db_session.get(Post, 1)
+#     assert post_db.id == 1
